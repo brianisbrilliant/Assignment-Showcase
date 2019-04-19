aa473f04b1752dca1e28aa767c86dd7976f370f9
Brian Foster <brianisbrilliant@gmail.com>
2019-04-19 11:47:29 -0500

    Initial Commit and GD1 portfolio

---
+++ Assets/Scripts/BuildLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildLevel : MonoBehaviour
+{
+	public Transform[] parents;
+	public float interval = 0.1f;
+	public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		player.SetActive(false);
+		foreach(Transform parent in parents) {
+			foreach(Transform child in parent) {
+				child.gameObject.SetActive(false);
+			}
+		}
+		StartCoroutine(Build());
+    }
+
+	IEnumerator Build() {
+		foreach(Transform parent in parents) {
+			foreach(Transform child in parent) {
+				child.gameObject.SetActive(true);
+				yield return new WaitForSeconds(interval);
+			}
+		}
+		player.SetActive(true);
+	}
+}
